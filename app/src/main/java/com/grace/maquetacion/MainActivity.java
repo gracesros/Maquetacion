c18b6e48ee97d8c6c2e020c2d7f4e27457e8735c
grace <gracesros@hotmail.com>
2019-10-14 20:10:28 -0500

    Initial commit

---
+++ app/src/main/java/com/grace/maquetacion/MainActivity.java
@@ -0,0 +1,44 @@
+package com.grace.maquetacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(),
+                        Seis.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(),
+                        Seis.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    }
+
